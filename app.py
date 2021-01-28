dd21e66925ee7d367a286b0b7ad4040e3ef5b70c
Kabir S. Tamari <kabirtamari@gmail.com>
2021-01-28 21:04:38 +0545

    added single thread one

--- app.py
+++ app.py
@@ -9,54 +9,10 @@ import threading
 import time
 
 
-THREAD_LIMIT = 10
 LOCATION = '/home/lurayy/mandala/compress/files'
 
-ACTIVE_COMPRESSIONS = 0
-COMPLETE_COUNT = 0
-TOTAL_COUNT = 0
 IMAGES = []
 LOG = None
-
-class Compressor(threading.Thread):
-
-    def __init__(self):
-        super(Compressor, self).__init__()
-        global LOG
-        self.log = LOG
-        global IMAGES
-        self.image = IMAGES.pop()
-    
-    def run(self):
-        im = None
-        try:
-            global ACTIVE_COMPRESSIONS
-            global COMPLETE_COUNT
-            self.log.write(f'Compressing : {self.image}\n')
-            ACTIVE_COMPRESSIONS = ACTIVE_COMPRESSIONS + 1 
-            im = Image.open(self.image)
-            os.remove(self.image)
-            im = im.convert('RGB')
-            img_byte = io.BytesIO()
-            im.save(img_byte, format='JPEG')
-            img_byte = img_byte.getvalue()
-
-            optimized_jpeg = pyguetzli.process_jpeg_bytes(img_byte)
-            im = Image.open(io.BytesIO(optimized_jpeg))
-            im.save(self.image,optimize=True, quality=90)
-            ext = str(self.image).split('.')
-            ext = ext[len(ext)-1]
-            if str(ext).lower() == "png":
-                cmd = f'python3 src/crunch.py {self.image}'
-                os.system(cmd)
-                os.remove(self.image)
-            ACTIVE_COMPRESSIONS = ACTIVE_COMPRESSIONS - 1
-            COMPLETE_COUNT = COMPLETE_COUNT + 1
-        except Exception as exp:
-            if im:
-                im.save(self.image,optimize=True, quality=100)
-            ACTIVE_COMPRESSIONS = ACTIVE_COMPRESSIONS - 1
-            self.log.write('error on : '+str(self.image)+'  error : '+str(exp)+'\n')
 
 class CompressionSystem():
     def __init__(self, log):
@@ -68,7 +24,6 @@ class CompressionSystem():
         IMAGES = set(Path(url).rglob("*.[pP][nN][gG]")).union(set(Path(url).rglob("*.[jJ][pP][gG]")).union(set(Path(url).rglob("*.[jJ][pP][eE][gG]"))))
         self.log.write(f'{len(IMAGES)} images found.\n')
         self.log.write('Starting Compression Process . . .\n')
-        TOTAL_COUNT = len(IMAGES)
         self.start_thread()
 
         images = set(Path(url).rglob("*.[pP][nN][gG]")).union(set(Path(url).rglob("*.[jJ][pP][gG]")).union(set(Path(url).rglob("*.[jJ][pP][eE][gG]"))))
@@ -79,18 +34,35 @@ class CompressionSystem():
     def start_thread(self):
         global IMAGES
         run = True
-        while run:
-            if self.can_run():
-                thread_s = Compressor()
-                thread_s.start()
-            if len(IMAGES) == 0:
-                run = False
+        i = 0
+        for image in IMAGES:
+            print(f'{i/len(IMAGES)*100} % Done\n')
+            self.compress(image)
+            i = i + 1
 
-    def can_run(self):
-        if ACTIVE_COMPRESSIONS < THREAD_LIMIT:
-            return True
-        else:
-            return False
+    def compress(self, image):
+        im = None
+        try:
+            self.log.write(f'Compressing : {image}\n')
+            im = Image.open(image)
+            os.remove(image)
+            im = im.convert('RGB')
+            img_byte = io.BytesIO()
+            im.save(img_byte, format='JPEG')
+            img_byte = img_byte.getvalue()
+            optimized_jpeg = pyguetzli.process_jpeg_bytes(img_byte)
+            im = Image.open(io.BytesIO(optimized_jpeg))
+            im.save(image,optimize=True, quality=90)
+            ext = str(image).split('.')
+            ext = ext[len(ext)-1]
+            if str(ext).lower() == "png":
+                cmd = f'python3 src/crunch.py {image}'
+                os.system(cmd)
+                os.remove(image)
+        except Exception as exp:
+            if im:
+                im.save(image,optimize=True, quality=100)
+            self.log.write('error on : '+str(image)+'  error : '+str(exp)+'\n')
 
     def clean_up(self, images):
         i = 0
