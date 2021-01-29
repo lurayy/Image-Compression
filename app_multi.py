572968f748d194fce7301b4ead58b6d9e9e980a4
Kabir S. Tamari <kabirtamari@gmail.com>
2021-01-29 14:09:19 +0545

    updated multi code

--- app_multi.py
+++ app_multi.py
@@ -9,7 +9,7 @@ import threading
 import time
 
 
-THREAD_LIMIT = 10
+THREAD_LIMIT = 2
 # LOCATION = '/home/ubuntu/torona/Image-Compression/media'
 LOCATION = '/home/lurayy/mandala/compress/temp'
 ACTIVE_COMPRESSIONS = 0
@@ -21,7 +21,12 @@ DATE = str(datetime.datetime.now())
 
 
 def log(msg):
-    with open('logs/'+str(DATE), 'a+') as f:
+    with open('logs/'+str(DATE)+'/main.log', 'a+') as f:
+        f.write(msg + '\n')
+
+
+def error_log(msg):
+    with open('logs/'+str(DATE)+'/error.log', 'a+') as f:
         f.write(msg + '\n')
 
 class Compressor(threading.Thread):
@@ -32,50 +37,52 @@ class Compressor(threading.Thread):
         self.image = IMAGES.pop()
     
     def run(self):
+        global COMPLETE_COUNT
+        global TOTAL_COUNT
         im = None
         try:
             global ACTIVE_COMPRESSIONS
-            global COMPLETE_COUNT
-            print('Compressing : ',self.image)
-            log(f'Compressing : {self.image}\n')
+            log(f'Compressing : {self.image}')
             ACTIVE_COMPRESSIONS = ACTIVE_COMPRESSIONS + 1 
             im = Image.open(self.image)
             im = im.convert('RGB')
             img_byte = io.BytesIO()
             im.save(img_byte, format='JPEG')
             img_byte = img_byte.getvalue()
-            log('Using pyguetzli : ', self.image)
             optimized_jpeg = pyguetzli.process_jpeg_bytes(img_byte)
             im = Image.open(io.BytesIO(optimized_jpeg))
             im.save(self.image,optimize=True, quality=90)
             ext = str(self.image).split('.')
             ext = ext[len(ext)-1]
             if str(ext).lower() == "png":
-                log('Using crnch : ',self.image )
+                log(f'Using crnch : {self.image}' )
                 cmd = f'python3 src/crunch.py {self.image}'
                 os.system(cmd)
                 os.remove(self.image)
             ACTIVE_COMPRESSIONS = ACTIVE_COMPRESSIONS - 1
             COMPLETE_COUNT = COMPLETE_COUNT + 1
+            log(f'Compression Complete : {self.image}')
+            log(f'--------------  {COMPLETE_COUNT/TOTAL_COUNT*100 } % -------------------')
         except Exception as exp:
             if im:
                 im.save(self.image,optimize=True, quality=100)
             ACTIVE_COMPRESSIONS = ACTIVE_COMPRESSIONS - 1
-            log('error on : '+str(self.image)+'  error : '+str(exp)+'\n')
+            error_log('error on : '+str(self.image)+'  error : '+str(exp)+'')
 
 class CompressionSystem():
 
     def start(self, url):
         global IMAGES
-        log('Finding all images . . . \n')
+        global TOTAL_COUNT
+        log('Finding all images . . . ')
         IMAGES = set(Path(url).rglob("*.[pP][nN][gG]")).union(set(Path(url).rglob("*.[jJ][pP][gG]")).union(set(Path(url).rglob("*.[jJ][pP][eE][gG]"))))
-        log(f'{len(IMAGES)} images found.\n')
-        log('Starting Compression Process . . .\n')
+        log(f'{len(IMAGES)} images found.')
+        log('Starting Compression Process . . .')
         TOTAL_COUNT = len(IMAGES)
         self.start_thread()
 
         images = set(Path(url).rglob("*.[pP][nN][gG]")).union(set(Path(url).rglob("*.[jJ][pP][gG]")).union(set(Path(url).rglob("*.[jJ][pP][eE][gG]"))))
-        log('Compression Complete, Cleaning up . . .\n')
+        log('Compression Complete, Cleaning up . . .')
         self.clean_up(images)
         return True
     
@@ -98,7 +105,7 @@ class CompressionSystem():
 
     def clean_up(self, images):
         i = 0
-        log('Starting Cleaning process\n')
+        log('Starting Cleaning process')
         for image in images:
             log(f'Total Cleaned : {i} , {float(i/len(images)*100)}Cleaning : {image}\n')
             new_name = (str(image).replace('-crunch', ''))
@@ -108,9 +115,10 @@ class CompressionSystem():
     
 if __name__ == "__main__":
     try:
+        os.mkdir('logs/'+str(DATE))
         compress = CompressionSystem()
         compress.start(LOCATION)
     except Exception as exp:
-        log(str(exp)+'\n')
+        error_log(str(exp)+'\n')
 
 
